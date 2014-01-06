7ae5578de67c999bd4e10ac53868098814e8954c
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2014-01-06 10:19:02 +0100

    Moved Exception to Vincent namespace and added header guard.

--- lib/include/vincent/exception.h
+++ lib/include/vincent/exception.h
@@ -1,7 +1,14 @@
+#ifndef VINCENT_EXCEPTION_H
+#define VINCENT_EXCEPTION_H
+
 #include <string>
 
-class Exception {
-  std::string message;
-public:
-  Exception(std::string message) : message(message) { }
-};
+namespace Vincent {
+  class Exception {
+    std::string message;
+  public:
+    Exception(std::string message) : message(message) { }
+  };
+}
+
+#endif
